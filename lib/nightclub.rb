7c12b9d9e42c007a292f8171c0d0f410ae4e3351
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-08-11 17:10:59 -0300

    subscription tests

--- lib/nightclub.rb
+++ lib/nightclub.rb
@@ -7,7 +7,22 @@ module Nightclub
       (imported - stored).map(&:save)
     end
 
-    def subscribe(user, party)
+    def subscribe(user)
+      raise ArgumentError unless user.valid?
+      Party.available(user).each do |party|
+        send_subscription(user, party) || next
+        party.emails << user.email
+        party.save
+      end
+    end
+
+    private
+
+    def import_parties
+      raise NotImplementedError
+    end
+
+    def send_subscription(user, party)
       raise NotImplementedError
     end
   end
--- test/unit/nightclub_base_test.rb
+++ test/unit/nightclub_base_test.rb
@@ -1,19 +1,38 @@
 require_relative '../test_helper' 
 
 class NightclubBaseTest < ActiveSupport::TestCase
-  setup { @club = Nightclub::Base.new }
+  setup do
+    @club = Nightclub::Base.new
+    @dude = User.new(:name => 'Dude', :email => 'dude@gmail.com')
+    @rockwork, @londoncalling, @fuckrehab = 3.times.map do
+      Party.new.tap { |p| p.stubs(:save); p.stubs(:destroy) }
+    end
+  end
 
   test '#sync!' do
-    neon, rehab, rockwork = 3.times.map{ Party.new }
-    Party.stubs(:all).returns [neon, rehab]
-    @club.stubs(:import_parties).returns [rehab, rockwork]
-    neon.expects(:destroy)
-    rockwork.expects(:save)
+    Party.stubs(:all).returns [@rockwork, @londoncalling]
+    @club.stubs(:import_parties).returns [@londoncalling, @fuckrehab]
+    @rockwork.expects(:destroy)
+    @fuckrehab.expects(:save)
     @club.sync!
   end
 
   test '#subscribe' do
-    user, party = 2.times.map { stub }
-    assert_raise(NotImplementedError) { @club.subscribe(user, party) }
+    Party.stubs(:available).returns [@londoncalling]
+    @club.stubs(:send_subscription).returns true
+    @club.subscribe(@dude)
+    assert @londoncalling.emails.include? 'dude@gmail.com'
+  end
+
+  test '#subscribe when user is invalid' do
+    @dude.stubs(:valid?).returns false
+    assert_raise(ArgumentError) { @club.subscribe(@dude) }
+  end
+
+  test '#subscribe when failed to send the subscription through' do
+    Party.stubs(:available).returns [@londoncalling]
+    @club.stubs(:send_subscription).returns false
+    @club.subscribe(@dude)
+    assert @londoncalling.emails.empty?
   end
 end
