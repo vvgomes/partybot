32825c13cdafdb6310c7d306b21e0f19f2c4386d
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-08-11 14:37:24 -0300

    sync! is an instance method

--- test/unit/nightclub_base_test.rb
+++ test/unit/nightclub_base_test.rb
@@ -3,7 +3,7 @@ require_relative '../test_helper'
 class NightclubBaseTest < ActiveSupport::TestCase
   setup { @club = Nightclub::Base.new }
 
-  test '.sync!' do
+  test '#sync!' do
     neon, rehab, rockwork = 3.times.map{ Party.new }
     Party.stubs(:all).returns [neon, rehab]
     @club.stubs(:import_parties).returns [rehab, rockwork]
