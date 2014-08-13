199cc52cd4bf5961f29d678a6ec8a3250840a239
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-08-13 18:14:40 -0300

    /subscriptions

--- partybot.rb
+++ partybot.rb
@@ -4,6 +4,16 @@ get '/' do
   status 200
 end
 
-post 'subscriptions' do
+post '/subscriptions' do
+  user = User.new(params[:user]) 
+  return status(400) unless user.valid?
+  if params[:party]
+    party = Party.where(:public_id => params[:party]).first
+    return status(400) unless party
+    Nightclub.current.subscribe(user, party)
+  else
+    Nightclub.current.bulk_subscribe(user, party)
+  end
+  status(200)
 end
 
