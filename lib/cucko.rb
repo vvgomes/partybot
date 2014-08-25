7831706148b7290befc739a14438230ac4132a36
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-08-25 12:41:46 -0300

    simplifies cucko

--- lib/cucko.rb
+++ lib/cucko.rb
@@ -3,8 +3,12 @@ require 'mechanize'
 class Cucko
   BASE_URL = 'http://cucko.com.br/nome_lista'
 
+  def initialize
+    @agent = Mechanize.new
+  end
+
   def import_parties
-    Mechanize.new.get("#{BASE_URL}/nomeLista").
+    @agent.get("#{BASE_URL}/nomeLista").
     search('select#evento option').map do |o|
       Party.new(:public_id => o['value'])
     end
@@ -16,6 +20,7 @@ class Cucko
       'email' => user.email,
       'idEvento' => party.public_id
     }
-    Net::HTTP.post_form(URI("#{BASE_URL}/gravaNomeLista"), data).code
+    uri = "#{BASE_URL}/gravaNomeLista"
+    @agent.post(uri, data).code
   end
 end
