865cefc9da0387edc8b50ebe99670af3164a5273
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-08-25 12:35:54 -0300

    simplifies lab

--- lib/lab.rb
+++ lib/lab.rb
@@ -3,8 +3,12 @@ require 'mechanize'
 class Lab
   BASE_URL = 'http://www.labpoa.com.br'
 
+  def initialize
+    @agent = Mechanize.new
+  end
+
   def import_parties
-    Mechanize.new.get("#{BASE_URL}/agenda").
+    @agent.get("#{BASE_URL}/agenda").
     search('#content ul.nav.nav-tabs.nav-stacked li a').map do |a|
       Party.new(:public_id => a['href'].delete('/'))
     end
@@ -15,12 +19,7 @@ class Lab
       'guests[0][email]' => user.email, 
       'guests[0][name]' => user.name
     }
-    Net::HTTP.post_form(post_uri(party), data).code
-  end
-
-  private
-
-  def post_uri(party)
-    URI("#{BASE_URL}/#{party.public_id}/add-guest")
+    uri = "#{BASE_URL}/#{party.public_id}/add-guest"
+    @agent.post(uri, data).code
   end
 end
