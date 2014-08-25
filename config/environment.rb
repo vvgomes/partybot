9b11cddb2f3d2053b17fcf9aad034bc9068623db
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-08-25 14:42:34 -0300

    drivers directory

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
 require 'mongoid'
 path = File.expand_path(File.dirname(__FILE__))
-Dir[File.join(path, '..', 'lib', '*.rb')].each{ |f| require f }
+Dir[File.join(path, '..', 'lib', '**/*.rb')].each{ |f| require f }
 Mongoid.load!(File.join(path, 'mongoid.yml'), ENV['RACK_ENV'])
