61ec996ae7d2dd94627f622ec7eb711fc9279711
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-08-28 20:35:21 -0300

    fixes beco driver and adds exceptions

--- lib/drivers/beco.rb
+++ lib/drivers/beco.rb
@@ -7,15 +7,6 @@ class Beco
     @agent = Mechanize.new
   end
 
-  def import_parties
-    @agent.get(BASE_URL).
-    links_with(:href => /^agenda\//).map(&:click).map do |page|
-      page.search('a[title="NOME NA LISTA"]').find do |a|
-        a['href'].match(/id=(?<id>\d+)/)['id']
-      end
-    end.compact.map{ |id| Party.new(:public_id => id) }
-  end
-
   def subscribe(user, party)
     data = {
       'nome' => user.name, 
@@ -26,5 +17,41 @@ class Beco
     path = '/resources/files/nomeLista.php?id=' 
     uri = [BASE_URL, path, party.public_id].join
     @agent.post(uri, data).code
+  end
+
+  def import_parties
+    home = @agent.get(BASE_URL)
+    rs_home = fetch_rs_home(home)
+    links = find_party_links(rs_home)
+    pages = links.map(&:click)
+    ids = pages.map do |page|
+      list_link = find_list_link(page)
+      find_public_id(list_link) if list_link
+    end.compact
+    ids.map{ |id| Party.new(:public_id => id) }
+  end
+
+  private
+
+  def fetch_rs_home(home)
+    link = home.link_with(:href => '/estado.php?estado=RS')
+    raise 'failed to fetch rs home' unless link
+    link.click
+  end
+
+  def find_party_links(rs_home)
+    links = rs_home.links_with(:href => /^agenda\//)
+    raise 'failed to find party links' if links.empty?
+    links
+  end
+
+  def find_list_link(party_page)
+    party_page.search('a[title="NOME NA LISTA"]').first
+  end
+
+  def find_public_id(list_link)
+    id = list_link['href'].match(/id=(?<id>\d+)/)['id']
+    raise 'failed to find public id' unless id
+    Integer(id).to_s # must be a number
   end
 end
