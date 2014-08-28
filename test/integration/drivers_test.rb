0a3684b7f43c20471c7d6ae8e842494699291d39
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-08-28 20:40:00 -0300

    small refactoring to tests

--- test/integration/drivers_test.rb
+++ test/integration/drivers_test.rb
@@ -1,15 +1,15 @@
 require_relative '../test_helper' 
 
 class DriversTest < ActiveSupport::TestCase
-  test 'beco driver can import more than one party' do
-    assert Beco.new.import_parties.size > 1
+  test 'beco driver can import parties' do
+    assert !(Beco.new.import_parties.empty?)
   end
 
-  test 'cucko driver can import more than one party' do
-    assert Cucko.new.import_parties.size > 1
+  test 'cucko driver can import parties' do
+    assert !(Cucko.new.import_parties.empty?)
   end
 
-  test 'lab driver can import more than one party' do
-    assert Lab.new.import_parties.size > 1
+  test 'lab driver can import parties' do
+    assert !(Lab.new.import_parties.empty?)
   end
 end
